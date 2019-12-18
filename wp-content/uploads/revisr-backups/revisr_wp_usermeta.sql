96e2baa7de773adfe78e75e6a0c96c447d210a4a
root <rene.mejia@santinosdigital.com>
2019-12-17 16:31:43 -0800

    Backed up the database with Revisr.

--- wp-content/uploads/revisr-backups/revisr_wp_usermeta.sql
+++ wp-content/uploads/revisr-backups/revisr_wp_usermeta.sql
@@ -20,12 +20,12 @@ CREATE TABLE `wp_usermeta` (
   PRIMARY KEY (`umeta_id`),
   KEY `user_id` (`user_id`),
   KEY `meta_key` (`meta_key`(191))
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 LOCK TABLES `wp_usermeta` WRITE;
 /*!40000 ALTER TABLE `wp_usermeta` DISABLE KEYS */;
-INSERT INTO `wp_usermeta` VALUES (1,1,'nickname','root'),(2,1,'first_name',''),(3,1,'last_name',''),(4,1,'description',''),(5,1,'rich_editing','true'),(6,1,'syntax_highlighting','true'),(7,1,'comment_shortcuts','false'),(8,1,'admin_color','fresh'),(9,1,'use_ssl','0'),(10,1,'show_admin_bar_front','true'),(11,1,'locale',''),(12,1,'wp_capabilities','a:1:{s:13:\"administrator\";b:1;}'),(13,1,'wp_user_level','10'),(14,1,'dismissed_wp_pointers',''),(15,1,'show_welcome_panel','1'),(16,1,'session_tokens','a:1:{s:64:\"1d6982fc68e2ca8dd73685e3e26521a6153746d763a177d93e97a52b95184bdd\";a:4:{s:10:\"expiration\";i:1576798867;s:2:\"ip\";s:3:\"::1\";s:2:\"ua\";s:114:\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36\";s:5:\"login\";i:1576626067;}}'),(17,1,'wp_dashboard_quick_press_last_post_id','4'),(18,1,'_astra_sites_gettings_started','1'),(19,1,'astra-theme-activation-nag','notice-dismissed');
+INSERT INTO `wp_usermeta` VALUES (1,1,'nickname','root'),(2,1,'first_name',''),(3,1,'last_name',''),(4,1,'description',''),(5,1,'rich_editing','true'),(6,1,'syntax_highlighting','true'),(7,1,'comment_shortcuts','false'),(8,1,'admin_color','fresh'),(9,1,'use_ssl','0'),(10,1,'show_admin_bar_front','true'),(11,1,'locale',''),(12,1,'wp_capabilities','a:1:{s:13:\"administrator\";b:1;}'),(13,1,'wp_user_level','10'),(14,1,'dismissed_wp_pointers',''),(15,1,'show_welcome_panel','1'),(16,1,'session_tokens','a:1:{s:64:\"1d6982fc68e2ca8dd73685e3e26521a6153746d763a177d93e97a52b95184bdd\";a:4:{s:10:\"expiration\";i:1576798867;s:2:\"ip\";s:3:\"::1\";s:2:\"ua\";s:114:\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36\";s:5:\"login\";i:1576626067;}}'),(17,1,'wp_dashboard_quick_press_last_post_id','4'),(18,1,'_astra_sites_gettings_started','1'),(19,1,'astra-theme-activation-nag','notice-dismissed'),(20,1,'wp_user-settings','mfold=o'),(21,1,'wp_user-settings-time','1576629068');
 /*!40000 ALTER TABLE `wp_usermeta` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
